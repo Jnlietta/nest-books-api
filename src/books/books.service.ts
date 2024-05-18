db8239c7b09b82339246c34eb5af11839c16c919
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-18 16:23:51 +0200

    Add methods getAll, getById in books.service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class BooksService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany({
+      include: { author: true },
+    });
+  }
+
+  public getById(id: Book['id']): Promise<Book | null> {
+    return this.prismaService.book.findUnique({
+      where: { id },
+      include: { author: true },
+    });
+  }
 }
