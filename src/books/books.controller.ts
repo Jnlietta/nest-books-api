59a349fc22f14cd22e01422505d6d0f6d778e648
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-18 16:28:38 +0200

    Add endpoints GET '/' and '/:id' in books.controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, ParseUUIDPipe } from '@nestjs/common';
 import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
+
+  @Get('/')
+  getAll() {
+    return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.booksService.getById(id);
+    if (!book) throw new NotFoundException('Book not found');
+    return book;
+  }
 }
