a15bc24666f18164796b00253e9aef1ee7036e20
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-18 16:47:12 +0200

    Add endpoint POST '/' in books.controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,15 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
   NotFoundException,
   Param,
   ParseUUIDPipe,
+  Post,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDTO } from './dtos/create-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -22,6 +25,11 @@ export class BooksController {
     const book = await this.booksService.getById(id);
     if (!book) throw new NotFoundException('Book not found');
     return book;
+  }
+
+  @Post('/')
+  create(@Body() bookData: CreateBookDTO) {
+    return this.booksService.create(bookData);
   }
 
   @Delete('/:id')
